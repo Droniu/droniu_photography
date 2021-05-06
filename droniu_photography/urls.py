ddc48d52e45ef7c373b5acdc63054bd5c9b47bef
Droniu <dron.official@yahoo.com>
2021-05-06 11:55:44 +0200

    basic frontend config

--- droniu_photography/urls.py
+++ droniu_photography/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('backend/', include('backend.urls'))
+    path('backend/', include('backend.urls')),
+    path('', include('frontend.urls')),
 ]
