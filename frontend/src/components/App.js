79b71f841d1e3c155180215ff67f0cb6a9169760
Droniu <dron.official@yahoo.com>
2021-05-07 21:02:50 +0200

    slides as objects

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,14 +1,46 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
 
-    render() {
-        return <h1>React Code</h1>;
+const slides = [
+    {
+        title: "a",
+        subtitle: "b",
+        desc: "c",
+        img: "https://wallpaperaccess.com/full/3850947.jpg",
+    },
+    {
+        title: "d",
+        subtitle: "e",
+        desc: "f",
+        img: "https://s2.best-wallpaper.net/wallpaper/1920x1200/1811/Blonde-girl-back-view-black-skirt-dusk-summer_1920x1200.jpg",
+    },
+];
+
+const initialSlide = {
+    slideIndex: 0
+}
+
+const slideReducer = (state, event) => {
+    if (event.type === 'NEXT') {
+        return {
+            ...state,
+            slideIndex: state.slideIndex + 1 % slides.length
+        }
     }
+}
+
+function App() {
+    return (
+    <div>
+        <h1>Hello World</h1>
+        {slides.map(slide => {
+            <div className="slide">{slide.title}</div>
+        })}
+        <button>Previous</button>
+        <button>Next</button>
+    </div>
+    );
 }
 
 const appDiv = document.getElementById("app");
