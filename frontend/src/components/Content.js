1f4ebf67ea0c9ef426f9719f2520ba25ef25e894
Droniu <dron.official@yahoo.com>
2021-05-09 10:43:10 +0200

    introducing slides

--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -1,10 +1,32 @@
 import React from "react";
 import './Content.css';
 
+const slides = [
+    {
+        title: 'Person A',
+        subtitle: 'A',
+        desc: 'Nobody likes A.',
+        img: './photos/1.jpg'
+    },
+    {
+        title: 'Person J',
+        subtitle: 'J',
+        desc: 'Some people like J.',
+        img: './photos/2.jpg'
+    },
+    {
+        title: 'Person T',
+        subtitle: 'T',
+        desc: 'Everyone loves T.',
+        img: './photos/3.jpg'
+    }
+]
+
 export function Content() {
     return (
+        <div id="videoDiv">
+            <img src="./static/images/photos/1.jpg" />
+        </div>
         
-        <video id="video" src="./static/images/printer.mp4" autoPlay muted loop />
-
         );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,22 +5,30 @@ import './Header.css';
 export function Header(props) {
     
     const large = props.large
-    var heightValue;
-    var transValue;
-    if (large){
-        heightValue = "100%"
-        transValue = "height 0s"
-    } else {
-        heightValue = "5%"
-        transValue = "height 1s"
-    }
+
 
     return (
     
-    <div id="cont"  style={{height: heightValue, transition: transValue}}>
-        <img src="./static/images/full_e2e2e2.svg"/>
-        <PropagateLoader color ="#e2e2e2" loading={large} />
-        <div style={{height: "20%"}}></div>
+    <div id="cont"  className={large ? "headerPre" : "headerAfter" }>
+        {
+            large ?
+            <React.Fragment>
+                <img src="./static/images/full_e2e2e2.svg"/>
+                <PropagateLoader color ="#e2e2e2"/>
+                <div style={{height: "20%"}}></div>
+            </React.Fragment>
+            :
+            <React.Fragment>
+                <div className="navItem">Events</div>
+                <div className="navItem">Portraits</div>
+                <div className="navItem">                
+                <img src="./static/images/full_e2e2e2.svg"/></div>
+                <div className="navItem">Services</div>
+                <div className="navItem">Contact</div>
+            </React.Fragment>
+
+        }
+
     </div>
     );
 }
