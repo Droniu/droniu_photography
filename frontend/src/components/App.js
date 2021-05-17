74b9b8577d11c9a67b1e2e646fc46a13fb14f28a
Droniu <dron.official@yahoo.com>
2021-05-17 11:10:55 +0200

    remove reduntant code

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,33 +4,6 @@ import { Header} from './Header.js';
 import { Content } from './Content.js';
 import './App.scss';
 
-const slides = [
-    {
-        title: "a",
-        subtitle: "b",
-        desc: "c",
-        img: "https://wallpaperaccess.com/full/3850947.jpg",
-    },
-    {
-        title: "d",
-        subtitle: "e",
-        desc: "f",
-        img: "https://s2.best-wallpaper.net/wallpaper/1920x1200/1811/Blonde-girl-back-view-black-skirt-dusk-summer_1920x1200.jpg",
-    },
-];
-
-const initialSlide = {
-    slideIndex: 0
-}
-
-const slideReducer = (state, event) => {
-    if (event.type === 'NEXT') {
-        return {
-            ...state,
-            slideIndex: state.slideIndex + 1 % slides.length
-        }
-    }
-}
 
 function App() {
     const [loading, setLoading] = useState(false);
