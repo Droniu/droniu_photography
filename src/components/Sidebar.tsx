6a063c49945412c02ed5b0060f75d70da229d2c4
Droniu <dron.official@yahoo.com>
2021-07-15 19:31:45 +0200

    Sidebar design

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './Sidebar.scss';
 import logo from '../logo.svg';
+import fb from '../fb_logo.svg';
+import ig from '../ig_logo.svg';
+import mail from '../mail_logo.svg';
 
 export function Sidebar() {
     return ( 
         <div className="sidebar">
-            <div>
+            <div className="logoContainer">
                 <img id="logo" src={logo} alt=""></img>
+            </div>
+            <div className="textContainer">
+                {/* text */}
+                <span>Droniu Photography</span>
+            </div>
+            <div className="socialContainer">
+                <img src={fb} alt="fb"></img>
+                <img src={ig} alt="fb"></img>
+                <img src={mail} alt="fb"></img>
+                {/* socials */}
             </div>
         </div>
     )
