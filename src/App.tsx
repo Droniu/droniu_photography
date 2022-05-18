3480aaeb8833547f89cf26c4a994ba6591460696
Droniu <dron.official@yahoo.com>
2022-05-18 15:15:46 +0200

    Factory reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import React, { useState } from 'react';
-import { Sidebar } from './components/Sidebar';
-import { Slides } from './components/Slides';
-import './App.scss'
-import {Section, PageState} from './lib/types'
-
-
 function App() {
-  
-  const initialPage: PageState = {
-    section: Section.Main
-  }
-  const [page, setPage] = useState<PageState>(initialPage)
   
   return (
     <div className="App">
-
-      <Sidebar pageMethod={setPage} pageState={page}/>
-      <Slides pageMethod={setPage} pageState={page}/>
     </div>
   );
 }
